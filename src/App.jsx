6268a4a55003749599ab3659fbdd17a825c19f79
Thais Calvi <thaispinheiro@hotmail.com>
2025-07-21 17:10:55 -0300

    conversor base com css

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    const [moeda, setMoeda] = useState("")
+    const [resultado, setResultado] = useState("")
+    const [trocar, setTrocar] = useState("reais")
+
+    const handleClickResultado =() => {
+      if (trocar === "reais"){
+        setResultado ((moeda/6).toFixed(2))
+      } else {
+        setResultado((moeda * 6).toFixed(2))
+      }
+    }
+
+    const handleClickTrocar = () => {
+      if (trocar === "reais") {
+        setTrocar("dollar")
+      } else {
+        setTrocar("reais")
+      }
+    }
+
+    const handleClickLimpar = () => {
+      setMoeda("")
+      setResultado("")
+      setTrocar("reais")
+    }
+
+
+  return (
+    <>
+     <div className="container">
+        <input type="number" 
+         className="input-moeda"
+        placeholder={trocar === "reais" ? "Digite o valor em R$" : "Digite o valor em $"}
+        value={moeda}
+        onChange={({target}) => setMoeda(target.value)}
+        />
+
+        <div className="resultado">
+          <p>{trocar === "reais" ? "Valor em $" : "Valor em R$"}{resultado}</p>
+        </div>
+
+       <button className="btn-trocar" onClick={handleClickTrocar}>Trocar</button>
+       <button className="btn-converter" onClick={handleClickResultado}>Converter</button>
+       <button  className="btn-limpar" onClick={handleClickLimpar}>Limpar</button>
+
+    </div>
+    </>
+  )
+}
+
+export default App
